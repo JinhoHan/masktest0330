21ae81bcdf5218cb1bc78552b3930118ce1fe504
jinhoHan <bedmil85@gmail.com>
2020-04-04 21:40:45 +0900

    Updates

---
+++ precache-manifest.664222689a5e196038c8a20e6fd4d4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8195e7bad5d6d725f52d6d16fc46b2",
+    "url": "/masktest0330/index.html"
+  },
+  {
+    "revision": "5a82c10a147b632313ef",
+    "url": "/masktest0330/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a951199ee6be9ffaa356",
+    "url": "/masktest0330/static/css/main.db2af7d5.chunk.css"
+  },
+  {
+    "revision": "5a82c10a147b632313ef",
+    "url": "/masktest0330/static/js/2.30f0e32d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/masktest0330/static/js/2.30f0e32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a951199ee6be9ffaa356",
+    "url": "/masktest0330/static/js/main.4b53249d.chunk.js"
+  },
+  {
+    "revision": "9f7d14f2d4e13997aeaa",
+    "url": "/masktest0330/static/js/runtime-main.d7285b0e.js"
+  }
+]);
