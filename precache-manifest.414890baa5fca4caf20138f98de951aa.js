14d5e803f83811517a9593845a6e0812daf46b4c
jinhoHan <bedmil85@gmail.com>
2020-04-06 13:52:24 +0900

    Updates

--- precache-manifest.414890baa5fca4caf20138f98de951aa.js
+++ precache-manifest.414890baa5fca4caf20138f98de951aa.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89728f56de0e81f6b21c8d9a161bc810",
+    "revision": "7a3707b7ca874dad7d81a35731494596",
     "url": "/masktest0330/index.html"
   },
   {
-    "revision": "bca3fa791ec95e00c75c",
+    "revision": "4221885ffb8ae74b928b",
     "url": "/masktest0330/static/css/2.89f067aa.chunk.css"
   },
   {
-    "revision": "cb092419362abdf7d0f9",
+    "revision": "cde2bd620b8610444767",
     "url": "/masktest0330/static/css/main.9844e629.chunk.css"
   },
   {
-    "revision": "bca3fa791ec95e00c75c",
-    "url": "/masktest0330/static/js/2.e0ef55dc.chunk.js"
+    "revision": "4221885ffb8ae74b928b",
+    "url": "/masktest0330/static/js/2.0adbef17.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/masktest0330/static/js/2.e0ef55dc.chunk.js.LICENSE.txt"
+    "url": "/masktest0330/static/js/2.0adbef17.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cb092419362abdf7d0f9",
-    "url": "/masktest0330/static/js/main.b6e4acd6.chunk.js"
+    "revision": "cde2bd620b8610444767",
+    "url": "/masktest0330/static/js/main.1bb3133b.chunk.js"
   },
   {
     "revision": "9f7d14f2d4e13997aeaa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/masktest0330/precache-manifest.3a03705047d5b1775e3045f576fb2906.js"
+  "/masktest0330/precache-manifest.414890baa5fca4caf20138f98de951aa.js"
 );
 
 self.addEventListener('message', (event) => {
